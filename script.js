f818ae5a005b16531fbfaeb569a32d7cfa9c7864
Marco <marcomorete73@hotmail.com>
2021-06-23 19:02:46 +0100

    improve scritps functional part

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
+var nav;
 var isOpen = false;
-window.addEventListener("click", function (e) {
+
+function clickOutside(e) {
   if (
     e.target !== document.querySelector("nav") &&
     e.target !== document.querySelector(".menu") &&
     isOpen
-  ) {
-    document.querySelector("nav").style.top = "-100vh";
-  }
-});
+  )
+    nav.style.top = "-100vh";
+}
+
+function addClickOutside() {
+  // console.log("added!");
+  window.removeEventListener("click", clickOutside);
+  window.addEventListener("click", clickOutside);
+}
+
+function removeClickOutside() {
+  // console.log("removed!");
+  window.removeEventListener("click", clickOutside);
+  isOpen && toggleMenu();
+}
+
 function toggleMenu() {
-  document.querySelector("nav").style.top = isOpen ? "-100vh" : "5rem";
+  // console.log(`toggle menu to ${!isOpen}`);
+  nav.style.top = isOpen ? "-100vh" : "5rem";
   isOpen = !isOpen;
+}
+
+function checkClickOutside() {
+  if (window.matchMedia("(min-width: 40em)").matches) removeClickOutside();
+  else addClickOutside();
+}
+
+window.onload = function() {
+  // console.log("loaded!")
+  nav = document.querySelector("nav");
+  checkClickOutside();
+}
+
+window.onresize = function() {
+  // console.log("resize!");
+  checkClickOutside();
 }
